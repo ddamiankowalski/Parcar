f61e3924b2108038acb88c16c5b76f00ae513667
Damian Kowalski <dkowalski.1997@gmail.com>
2022-06-02 02:29:32 +0200

    next changes

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:async';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const MyApp());
 }
 
@@ -27,9 +33,9 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: <Widget>[
-        Expanded(flex: 2, child: topStack()),
+        Expanded(flex: 4, child: topStack()),
         Expanded(
-          flex: 2,
+          flex: 3,
           child: bottomSection(context),
         )
       ],
@@ -90,6 +96,40 @@ class _TopTextStack extends State<TopTextStack> {
             decoration: const BoxDecoration(
                 image: DecorationImage(
                     image: AssetImage("assets/la.jpg"), fit: BoxFit.cover))),
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              gradient: LinearGradient(
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  colors: [
+                    Colors.black.withOpacity(0.3),
+                    Colors.black.withOpacity(0.3),
+                  ],
+                  stops: const [
+                    0.0,
+                    0.7
+                  ])),
+        ),
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              gradient: LinearGradient(
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  colors: [
+                    primaryColor.withOpacity(0.4),
+                    primaryColor.withOpacity(0.4),
+                  ],
+                  stops: const [
+                    0.0,
+                    1.0
+                  ])),
+        ),
         IgnorePointer(
             ignoring: true,
             child: Column(
@@ -101,14 +141,14 @@ class _TopTextStack extends State<TopTextStack> {
                     SvgPicture.asset(
                       'assets/logo.svg',
                       semanticsLabel: 'Parcar Logo',
-                      height: 25,
-                      color: primaryColor,
+                      height: 27,
+                      color: Colors.white,
                     ),
-                    const SizedBox(width: 10),
+                    const SizedBox(width: 13),
                     RichText(
                       text: const TextSpan(
                         style: TextStyle(
-                          fontSize: 36,
+                          fontSize: 40,
                           fontFamily: 'Poppins',
                           fontWeight: FontWeight.w600,
                           color: Colors.white,
@@ -117,7 +157,7 @@ class _TopTextStack extends State<TopTextStack> {
                           TextSpan(text: 'Par'),
                           TextSpan(
                               text: 'car',
-                              style: TextStyle(fontWeight: FontWeight.w600)),
+                              style: TextStyle(fontWeight: FontWeight.w400)),
                         ],
                       ),
                     )
@@ -131,14 +171,14 @@ class _TopTextStack extends State<TopTextStack> {
                         style: TextStyle(
                           fontSize: 14,
                           fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w300,
+                          fontWeight: FontWeight.w500,
                           color: Colors.white,
                         ),
                         children: <TextSpan>[
                           TextSpan(text: 'Find yourself a perfect spot to '),
                           TextSpan(
                             text: 'parc!',
-                            style: TextStyle(fontWeight: FontWeight.w600),
+                            style: TextStyle(fontWeight: FontWeight.w700),
                           ),
                         ],
                       ),
@@ -377,9 +417,10 @@ Widget bottomSection(context) {
                         ),
                         onTap: () {
                           Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignInPage()));
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignInPage()),
+                          );
                         },
                         child: const Padding(
                             padding: EdgeInsets.only(top: 18, bottom: 18),
@@ -438,22 +479,191 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPage extends State<SignInPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+
+  signIn(id, message) async {
+    var response =
+        await http.get(Uri.parse("http://192.168.1.12:3000/api/users"));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
+        body: Column(
+      children: <Widget>[
+        const SizedBox(
+          height: 80,
         ),
-      ),
-    );
+        Row(
+          children: <Widget>[
+            const SizedBox(width: 30),
+            GestureDetector(
+              onTap: () => {Navigator.pop(context)},
+              child: SvgPicture.asset(
+                'assets/left.svg',
+                semanticsLabel: 'leftArrow',
+                height: 25,
+                color: Colors.black,
+              ),
+            ),
+            const Expanded(
+                child: Text('Welcome back',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black))),
+            const SizedBox(width: 30),
+          ],
+        ),
+        const SizedBox(height: 70),
+        Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                FractionallySizedBox(
+                  widthFactor: 0.8,
+                  child: TextFormField(
+                    autofocus: true,
+                    style: const TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16),
+                    enableSuggestions: false,
+                    decoration: InputDecoration(
+                      labelText: "Your email",
+                      floatingLabelStyle: const TextStyle(
+                        color: Color.fromARGB(255, 104, 103, 119),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
+                      labelStyle: const TextStyle(
+                          color: Color.fromARGB(255, 104, 103, 119),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide:
+                              BorderSide(color: primaryColor, width: 1.0)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                              color: Color.fromARGB(10, 0, 0, 0), width: 0.0)),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.8,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+
+                      return null;
+                    },
+                    style: const TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16),
+                    enableSuggestions: false,
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      floatingLabelStyle: const TextStyle(
+                          color: Color.fromARGB(255, 104, 103, 119),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16),
+                      labelStyle: const TextStyle(
+                          color: Color.fromARGB(255, 104, 103, 119),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide:
+                              BorderSide(color: primaryColor, width: 1.0)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                              color: Color.fromARGB(10, 0, 0, 0), width: 0.0)),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 55,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.8,
+                  child: Container(
+                    height: 56,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                      color: primaryColor,
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                          customBorder: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          onTap: () async {
+                            if (_formKey.currentState!.validate()) {
+                              setState(() {
+                                _isLoading = true;
+                              });
+                            }
+
+                            String result = await signIn('id', 'sa');
+                            print(result);
+
+                            if (result != null) {
+                              setState(() {
+                                _isLoading = false;
+                              });
+                            }
+                          },
+                          child: Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 18, bottom: 18),
+                              child: !_isLoading
+                                  ? const Text('Sign in',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white))
+                                  : spinkit)),
+                    ),
+                  ),
+                ),
+              ],
+            ))
+      ],
+    ));
   }
 }
 
 Color primaryColor = const Color.fromARGB(255, 85, 74, 240);
+const spinkit = SpinKitRing(
+  color: Colors.white,
+  lineWidth: 2,
+  size: 20.0,
+);
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
